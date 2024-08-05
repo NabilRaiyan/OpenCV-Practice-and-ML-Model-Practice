59dcad2206c4eeb950b76f391a84e94ea2034f12
NabilRaiyan <rraiyan77@gmail.com>
2024-08-05 20:48:26 +0600

    basic open cv

--- basic.py
+++ basic.py
@@ -14,8 +14,24 @@ blurImg = cv.GaussianBlur(img, (9, 9), cv.BORDER_DEFAULT)
 cv.imshow("Blured image", blurImg)
 
 
-# Edge cascade
+# Edge cascade finding edges in an image
+canny = cv.Canny(img, 125, 175)
+cv.imshow('Canny Edge', canny)
 
 
+# Dilating the image
+dilated = cv.dilate(canny, (3, 3), iterations=1)
+cv.imshow('Dialated Image',dilated)
+
+# Eroding
+eroded = cv.erode(dilated, (3, 3), iterations=1)
+cv.imshow('Eroded Image',eroded)
+
+# Resize or crop an image
+resizedImg = cv.resize(img, (600, 500))
+cv.imshow('Resized image', resizedImg)
+
+croppingImg = img[100:200, 200:400]
+cv.imshow("Cropped Image", croppingImg)
 
 cv.waitKey(0)
