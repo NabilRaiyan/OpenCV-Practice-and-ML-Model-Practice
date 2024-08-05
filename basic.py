98f59570c3c272b9ddf3a96feb809ca3e054ce73
NabilRaiyan <rraiyan77@gmail.com>
2024-08-05 20:25:53 +0600

    basic open cv

--- basic.py
+++ basic.py
@@ -1,7 +1,21 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('Photos/img-3.png')
+img = cv.imread('Photos/Cat.jpeg')
 cv.imshow('Cat', img)
+
+
+# convert and image to gray scale
+grayImg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray Scale', grayImg)
+
+# Blur
+blurImg = cv.GaussianBlur(img, (9, 9), cv.BORDER_DEFAULT)
+cv.imshow("Blured image", blurImg)
+
+
+# Edge cascade
+
+
 
 cv.waitKey(0)
